7aaba80c81d24b3f131a6b197bed147463f6b6f0
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-07-19 13:20:16 +0500

    Update UK Salary Calculator After Tax.R
    Optimised script:
    
    • Reduced excessive lines
    • Fixed bug leading to showing monthly salary as NA for salary below £12,570
    • Moved SPRINTF function out of IF/ELSE statements allowing to leave only 1 function instead of 4
    • Simplified mathematical calculations
    • Added ROUND to monthly salaries

--- UK Salary Calculator After Tax.R
+++ UK Salary Calculator After Tax.R
@@ -1,24 +1,17 @@
-salary.calculator.uk <- function(x){ # Calculator for UK salaries
+salary.calculator.uk <- function(x){ # Calculator for UK salaries after tax
   
-  if (isTRUE(x <= 12570)){ # When salary is below or equal to £12,570
-    
-    sprintf("Your salary would be %s annually or %s monthly after tax", x) } 
+  if (isTRUE(x <= 12570)){ y <- c(x, round(x / 12, 2)) } # =< £12,570
   
   else if (isTRUE(x >= 12571) && isTRUE(x <= 50270)){ # > £12,571 & < £50,270
     
-    sprintf("Your salary would be %s annually or %s monthly after tax",
-            12571 + (x - 12571) * .8, (12571 + (x - 12571) * .8) / 12) }
+    y <- c(2514.2 + x * .8, round((2514.2 + x * .8) / 12, 2)) } 
   
   else if (isTRUE(x >= 50271) && isTRUE(x <= 125140)){ # > £50,271 & < £125,140
     
-    sprintf("Your salary would be %s annually or %s monthly after tax",
-            12571 + (50270 - 12571) * .8 + (x - 50271) * .6,
-            (12571 + (50270 - 12571) * .8 + (x - 50271) * .6) / 12) } 
+    y <- c(12567.6 + x * .6, round((12567.6 + x * .6) / 12, 2)) } 
   
-  else { # > £125,140
-    
-    sprintf("Your salary would be %s annually or %s monthly after tax",
-            12571 + (50270-12571) * .8 + (125140-50271) * .6 + (x-125140) * .55,
-            (12571+(50270-12571)*.8+(125140-50271)*.6+(x-125140)*.55) / 12) }
+  else { y <- c(18824.6 + x*.55, round((18824.6 + x*.55)/12, 2)) } # > £125,140
+  
+  sprintf("Salary would be %s annually or %s monthly after tax", y[1], y[2])
 }
-salary.calculator.uk(60000) # Test
+salary.calculator.uk(152000) # Test
