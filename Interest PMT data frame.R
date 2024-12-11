55c3ace9bce387ab3a42a77f2d7b47dcce9e497e
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-12-11 16:27:51 +0500

    Update Interest PMT data frame.R
    Rewrote script:
    
    • Added option to calculate average increase of interest payment per one percent
    • Added option to choose maximum interest rate

--- Interest PMT data frame.R
+++ Interest PMT data frame.R
@@ -1,14 +1,14 @@
-# Function to get df with interest and monthly mortgage payment
-pmt.df.interest <- function(a, t) { l <- NULL
+pmt.df.interest <- function(a, t, overpayment = F, max = 0.3) { 
   
-  for (r in seq(0, .5, .01)){ l <- rbind(l, (a*r/12)/(1-(1 + r/12)^(-t*12))) }
+  l <- NULL # Data Frame of interest & payment
+
+  for (r in seq(0,max,.01)){ if (r==0){ l<-rbind(l,cbind(0,a/(t*12))) } else {
+    
+      l <- rbind(l, cbind(r * 100, (a * r/12)/(1 - (1 + r/12)^(-t * 12)))) } }
   
-  l <- cbind(seq(0, .5, .01), l) # Add data frame with interests
+  colnames(l) <- c("Interest (%)", "Monthly Payment") # Column names
+  rownames(l) <- seq(nrow(l)) # Numeric Row Names
   
-  l[1,2] <- a / (t * 12) # Insert right monthly amount
-  
-  colnames(l) <- c("Interest", "Amount")
-  
-  l # Display
+  ifelse(overpayment == T, summary(lm(l[,2] ~ l[,1]))$coefficient[2], return(l))
 }
-pmt.df.interest(200000, 30) # Test
+pmt.df.interest(200000, 30, T, 0.1) # Data frame or average overpayment amount
