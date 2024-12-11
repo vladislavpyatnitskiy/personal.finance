c5b965e38a548a5fe1a27a6b50a685b27dacb05d
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-12-11 16:40:37 +0500

    Update Mortgage Payment Calculator.R
    Reduced part for mortgage monthly payment

--- Mortgage Payment Calculator.R
+++ Mortgage Payment Calculator.R
@@ -1,10 +1,6 @@
-# Function to calculate monthly mortgage payment
-mortgage.payment.calculator <- function(a, r, t, c = "£") {
+mortgage.pmt.calculator <- function(a,r,t,c="£"){ # mortgage payment calculator
   
-  m.payment <- round((a * r / 12) / (1 - (1 + r / 12) ^ (-t * 12)), 2)
-  
-  sprintf("Payment for %s %s mortgage for %s year at %s%% is %s%s a month",
-          c, a, t, r * 100, c, m.payment)
+  sprintf("Payment for %s%s mortgage for %s year at %s%% is %s%s a month",
+          c, a, t, r * 100, c, round((a*r/12) / (1 - (1 + r/12)^(-t*12)), 2))
 }
-# Test
-mortgage.payment.calculator(200000, 0.045, 30)
+mortgage.pmt.calculator(120000, 0.05, 30) # Test
