8e92ed2641b05790bdf73f15ea0989cadf4877fe
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-04 11:15:46 +0500

    Update Yearly Contributions to Retire.R
    Added output:
    
    • Script calculates Future value of accumulated contributions
    • Output also includes data frame with columns of years to retirement, year of contribution, expected contributions and future values of these contributions

--- Retirement Calculators/Yearly Contributions to Retire.R
+++ Retirement Calculators/Yearly Contributions to Retire.R
@@ -1,4 +1,4 @@
-retire.contributions <- function(C, rd, t, rr){
+retire.contributions <- function(C, rd, t, rr, y = NULL){
   
   # Input:
   # C - amount you want to save in total 
@@ -8,6 +8,23 @@ retire.contributions <- function(C, rd, t, rr){
   
   # Output: Amount you have to save each year
   
-  C * (1 + rd) ^ t / (1 + rr) / ((1 - (1 + rr) ^ t) / (1 - (1 + rr)))
+  if (is.null(y)){ y = as.numeric(strsplit(as.character(Sys.Date()),
+                                           split = "-")[[1]][1]) }
+  R <- 1 + rr # Return Rate
+  
+  payment = C * (1 + rd) ^ t / R / ((1 - R ^ t) / (1 - R))
+  
+  D = cbind(seq(t, 1), seq(y, y + t - 1), (C / t), (C / t) * R ^ seq(t, 1))
+  
+  rownames(D) <- seq(nrow(D)) # Standard Numeric Row Names
+  
+  colnames(D) <- c("Years to retirement", "Year", "Expected Contributions",
+                   "Future Values of Contributions")
+  
+  df <- list(sum(D[,4]), payment, D) # Put all in a single list
+  
+  names(df) <- c("Future Value", "Yearly Payment", "Table") # Names
+  
+  df # Display
 }
-retire.contributions(1000000, .03, 40, .05) # Test
+retire.contributions(1000000, .03, 40, .05, 2024) # Test
