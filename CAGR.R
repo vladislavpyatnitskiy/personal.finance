06b9041499ef46ca966a0b44cd3944106f8e6f91
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-05 13:35:08 +0500

    Update CAGR.R
    Optimised script:
    
    • Reduced excessive lines
    • Eliminated WHILE loop

--- CAGR.R
+++ CAGR.R
@@ -2,11 +2,7 @@ CAGR <- function(x){ # Compound Annual Growth Rate
   
   l <- NULL # Store values
   
-  for (n in 1:length(x)){ while (x[n] != x[length(x)]){ # Add values
-    
-      l <- c(l, round((x[n + 1] / x[1]) ^ (1 / n) - 1, 2)) # Till last number
-    
-    break } } # Stop when cycle is over
+  for (n in 1:(length(x) - 1)) l <- c(l, round((x[n+1] / x[1]) ^ (1/n) - 1, 2))
   
   l # Display
 }
