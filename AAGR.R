81c65bffb26a84562bdb95b2ab6fb7dafc368845
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2025-07-06 11:19:57 +0500

    Update AAGR.R
    Optimised script:
    
    • Reduced excessive lines
    • Eliminated WHILE loop

--- AAGR.R
+++ AAGR.R
@@ -2,12 +2,8 @@ AAGR <- function(x){ # Average Annual Growth Rate
   
   l <- NULL # Store values
   
-  for (n in 1:length(x)){ while (x[n] != x[length(x)]){ # Add values
+  for (n in 1:(length(x) - 1)) l <- c(l, round((x[n + 1] / x[n]) - 1, 2))
     
-      l <- c(l, round((x[n + 1] / x[n]) - 1, 2)) # Till last number
-    
-      break } } # Stop when cycle is over
-  
   l # Display
 }
 AAGR(c(1000, 1200, 900, 1600, 1800, 2250)) # Test
